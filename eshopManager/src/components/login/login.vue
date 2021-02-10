1af1b34e441d6297fa90b502c5968c97d2c45c82
zxs <zhangxs@ccccit.com>
2021-02-10 13:44:31 +0800

    登录样式调整

--- eshopManager/src/components/login/login.vue
+++ eshopManager/src/components/login/login.vue
@@ -1,10 +1,45 @@
 <template>
-  <div>denglu</div>
+  <div class="login-wrap">
+    <el-form
+      class="login-form"
+      label-position="top"
+      label-width="80px"
+      :model="formdata"
+    >
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input
+          placeholder="请输入密码"
+          v-model="formdata.password"
+        ></el-input>
+      </el-form-item>
+      <el-button class="login-btn">登录</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      formdata: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+};
 </script>
 
 <style scoped>
+.login-wrap {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #324152;
+}
 </style>
--- eshopManager/src/main.js
+++ eshopManager/src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import '@/assets/css/reset.css'
 //生产环境切换
 Vue.config.productionTip = false
 Vue.use(ElementUI);
