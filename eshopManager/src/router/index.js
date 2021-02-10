b63d733d5c93d367534c8912f6255606cf1456d7
zxs <zhangxs@ccccit.com>
2021-02-10 11:00:12 +0800

    新建登录分支-路由配置和组件

--- eshopManager/src/router/index.js
+++ eshopManager/src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/login/login.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    }
 
   ]
 })
